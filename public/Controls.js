ab76fe2ae6708688eb9daca7c12f694c3f8ab867
Andrei Dragomir <dragomirandy900@yahoo.com>
2018-11-15 11:40:40 +0200

    Update Controls.js

--- public/Controls.js
+++ public/Controls.js
@@ -7,11 +7,22 @@ function Controls(){
     if(keyWentDown('w'))
       player.jump();
     if(keyWentDown('r'))
-      player.clone();
-  
+      player.cloning();
+
+  if(keyWentDown('c'))
+  {
+    var bullet = createSprite(player.s.position.x, player.s.position.y, 50, 20);
+    if(camera.mouseX < player.s.position.x)
+        bullet.velocity.x = -5 - player.s.getSpeed();
+    else bullet.velocity.x = 5 + player.s.getSpeed();
+
+    bullet.life = 50;
+    bullets.add(bullet);
+  }
+
   if(keyDown('f'))
       background(255);
-  
+
   //Player 2
     if(keyDown('k'))
       players[1].move(moveSpeed);
