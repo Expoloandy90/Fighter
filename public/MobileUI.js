f33ff313d3b024eedbee8b65786818a595bff69a
Andrei Dragomir <dragomirandy900@yahoo.com>
2018-12-09 21:33:14 +0200

    Mobile Jump

--- public/MobileUI.js
+++ public/MobileUI.js
@@ -1,18 +1,29 @@
 var valoare = 0;
+var jumpStop = 0;
 function UpdateMobileUI(){
 	fill(valoare);
 	rect(50, windowHeight - 100 ,50 ,50, 5);
 	rect(100, windowHeight - 100 ,50 ,50, 5);
+	rect(75, windowHeight - 150 ,50 ,50, 5);
 	for(var i=0; i<touches.length; i++)
 	{
-		if(touches[i].x > 50 && touches[i].x <100){
+		if(touches[i].x > 50 && touches[i].x <100 && touches[i].y > windowHeight - 100 && touches[i].y < windowHeight - 50){
 			player.move(-player.moveSpeed);
 			valoare = 100;
 		}
-		if(touches[i].x > 100 && touches[i].x <150){
+		if(touches[i].x > 100 && touches[i].x <150 && touches[i].y > windowHeight - 100 && touches[i].y < windowHeight - 50){
 			player.move(player.moveSpeed);
 			valoare = 200;
 		}
+		if(touches[i].x > 75 && touches[i].x <125 && touches[i].y > windowHeight - 150 && touches[i].y < windowHeight - 100){
+			if(jumpStop == 0)
+			player.jump();
+			jumpStop = 1;
+			valoare = 200;
+		}
 	}
-	//text(frameRate() , player.s.position.x, player.s.position.y);
+}
+
+function touchEnded() {
+  jumpStop = 0;
 }
